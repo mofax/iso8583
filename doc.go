d8620421909a3c87e20cbeb99b2bf2a98b9a23e6
Alloys Mila <mila.alloys@gmail.com>
2017-04-06 10:59:45 +0300

    add doc.go

---
+++ doc.go
@@ -0,0 +1,13 @@
+// Copyright 2017 Alloys Mila. All rights reserved.
+// This source code is lincenced under Apache-2
+
+/*
+ISO 8583 is a financial industry messaging format.
+when you make a purchase using your card, that transaction will probably reach you bank encoded
+in ISO 8583 format.
+
+This package provides tools for packing and unpacking those kinds of
+messages
+*/
+
+package iso8583
